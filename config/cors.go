8451c8798809bd0ee9f27bbd74b0169036f9a700
rrq-dev <raihanhermawan85@gmail.com>
2025-07-21 00:27:32 +0700

    feat: update CORS configuration to include new allowed origin for production

--- config/cors.go
+++ config/cors.go
@@ -1,12 +1,11 @@
 package config
 
-var AllowedOrigins = []string {
-		"http://localhost:1010",
-		"http://localhost:5173",
-	}
+var AllowedOrigins = []string{
+	"http://localhost:1010",
+	"http://localhost:5173",
+	"https://embeck.onrender.com",
+}
 
 func GetAllowedOrigins() []string {
 	return AllowedOrigins
-}	
-
-
+}
