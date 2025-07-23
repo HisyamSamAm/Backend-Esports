1dc6f123eebc2cc7fac6be9607296e4c2f1eb465
rrq-dev <raihanhermawan85@gmail.com>
2025-07-23 12:47:01 +0700

    feat: update CORS configuration by commenting out old origin and ensuring deployment URLs are included

--- config/cors.go
+++ config/cors.go
@@ -3,7 +3,10 @@ package config
 var AllowedOrigins = []string{
 	"http://localhost:1010",
 	"http://localhost:5173",
-	"https://embeck.onrender.com",
+	// "https://embeck.onrender.com",
+	"https://backend-esports.up.railway.app", // deploy
+	"https://esports-app.netlify.app", // deploy
+
 }
 
 func GetAllowedOrigins() []string {
